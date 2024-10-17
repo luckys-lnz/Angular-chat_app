eceb53b0bb58fce2000c0a601c0e479c0c965fd9
luckys-lnz <lalucky22@gmail.com>
2024-10-17 13:57:49 +0000

    ...MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatTopComponent } from './chat-top/chat-top.component';
+import { DashboardOverviewComponent } from './dashboard-overview/dashboard-overview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatTopComponent, DashboardOverviewComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular-chat_app';
